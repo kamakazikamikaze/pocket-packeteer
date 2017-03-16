58368d4ca2d990e17fc0e628ee603438bd6e55ee
Kent Coble <kamakazikamikaze@gmail.com>
2017-03-16 11:00:22 -0600

    Temporary commit for assignment requirements

--- packeteer/__init__.py
+++ packeteer/__init__.py
@@ -1,6 +1,15 @@
 from __future__ import print_function
-from scapy import *
-from . import scanner
+from collections import defaultdict
+from netaddr import iter_iprange, IPNetwork
+from os import pathsep
+from os.path import abspath
+# from scapy import *
+# from scapy.sendrecv import send, sr1
+# from scapy.layers.inet import IP, TCP, UDP, ICMP
+# from scapy.plist import SndRcvList
+from scapy.all import *
+from sys import exit
+from functools import reduce
 
 DEFAULT_PORTS = [port for port in range(1, 4096)]
 
@@ -19,17 +28,17 @@ def scan(target, proto, ports=None, timeout=10, **kwargs):
     :type ports: iterable(int or str)
     :param kwargs: Keyword arguments to pass to ``scapy.sendrecv.sr1``
     :return: Responses
-    :rtype: scapy.plist.SndRcvList
+    :rtype: list(tuple(port, scapy.plist.SndRcvList))
     '''
     if timeout <= 0:
         timeout = 10
     ip = IP(dst=target)
-    response = []
+    responses = []
     for port in ports:
-        response.append(
+        responses.append(
             (port, sr1(ip / proto(dport=port), timeout=timeout, **kwargs))
         )
-    return response
+    return responses
 
 
 def scanICMP(targets, timeout=10, **kwargs):
@@ -40,7 +49,7 @@ def scanICMP(targets, timeout=10, **kwargs):
     :type targets: str or iterable(str)
     :param kwargs: Keyword arguments to pass to ``scapy.sendrecv.sr1``
     :return: Ping results
-    :rtype: tuple(list(str), list(str), list(scapy.layers.inet.IP))
+    :rtype: tuple(list(str), list(scapy.layers.inet.IP))
     '''
     if timeout <= 0:
         timeout = 10
@@ -56,6 +65,7 @@ def scanICMP(targets, timeout=10, **kwargs):
             dead.append(target)
         else:
             alive.append(target)
+    # We'll allow the dead to be returned
     return alive, dead, responses
 
 
@@ -71,10 +81,15 @@ def scanUDP(target, ports=None, timeout=10, **kwargs):
     '''
     if timeout <= 0:
         timeout = 10
-    answered, unanswered = scan(target, UDP, ports, timeout, **kwargs)
+    responses = scan(target, UDP, ports, timeout, **kwargs)
     # TODO: Implement a proper logger
+    answered = filter(lambda r: r[1] is not None, responses)
+    unanswered = filter(lambda r: r[1] is None, responses)
     if not answered:
-        print('No ICMP responses were received; cannot verify if target is up')
+        print(
+            'No ICMP responses were received for',
+            target,
+            'in UDP scan. Cannot verify if target is up')
     return unanswered
 
 
@@ -90,7 +105,8 @@ def scanTCP(target, ports=None, timeout=10, **kwargs):
     '''
     if timeout <= 0:
         timeout = 10
-    return scan(target, TCP, ports, timeout, **kwargs)[0]
+    return filter(lambda r: r[1] is not None, scan(
+        target, TCP, ports, timeout, **kwargs))
 
 
 def scanSYN(target, ports=None, timeout=10, **kwargs):
@@ -108,8 +124,220 @@ def scanSYN(target, ports=None, timeout=10, **kwargs):
     ip = IP(dst=target)
     results = []
     for port in ports:
-        response = sr1(ip / TCP(dport=port, flags='S'))
-        if response and response.flags == 0x14:
-            results.append(response)
-            send(ip / TCP(dport=port, flags='R'))
+        response = sr1(ip / TCP(dport=port, flags='S'),
+                       timeout=timeout, **kwargs)
+        # If we're received a SYN+ACK, save the result for later
+        if response and response[TCP].flags == 'SA':
+            results.append((port, response))
+            send(ip / TCP(dport=port, flags='R'), **kwargs)
     return results
+
+
+def parse_targets(targets):
+    r'''
+    Expand the parameter into individual IP addresses
+
+    .. todo:: Make this recursive. Too hungry right now to think
+
+    :param targets: IP address(es) of host to target
+    '''
+    targets = [targets] if isinstance(targets, str) else targets
+    temptargets = []
+    for target in targets:
+        # Filter blanks
+        temptargets.extend(
+            filter(
+                lambda x: x is not '',
+                target.strip().split(',')))
+    targets = temptargets
+    temptargets = []
+    for target in targets:
+        if '/' in target:
+            temptargets.extend(list(map(str, IPNetwork(target))))
+        elif '-' in target:
+            start, end = target.split('-')
+            temptargets.extend(list(map(str, iter_iprange(start, end))))
+        else:
+            temptargets.append(target)
+    return temptargets
+
+
+def parse_ports(ports):
+    r'''
+    Expand the parameter into a list of individual ports
+
+    :param ports: All ports to scan
+    :type ports: str or int or list(str or int)
+    :rtype: list(int)
+    '''
+    ports = [ports] if isinstance(ports, str) else ports
+    tempports = []
+    for port_range in ports:
+        tempports.extend(
+            filter(
+                lambda x: x is not '',
+                port_range.strip().split(',')))
+    ports = tempports
+    tempports = []
+    for port_range in ports:
+        if isinstance(port_range, int):
+            tempports.append(port_range)
+        elif '-' in port_range:
+            start, end = port_range.split('-')
+            tempports.extend(range(int(start), int(end) + 1))
+        else:
+            tempports.append(int(port_range))
+    return tempports
+
+scan_actions = {
+    'icmp': scanICMP,
+    'udp': scanUDP,
+    'tcp': scanTCP,
+    'syn': scanSYN
+}
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser(
+        description=(
+            'Assignment 3 deliverable. A full Scanner class with '
+            'multiprocessing capability will be created later. This '
+            'invocation is to help you get started. Specify targets in the '
+            'CLI or in a file with a list of hosts or both.'))
+    parser.add_argument('-i', '--input-file',
+                        help=('File containing hosts. One per line. CIDR and '
+                              'ranges are accepted'))
+    parser.add_argument('-t', '--targets', nargs='+',
+                        help=(
+                            'Targets to ping. A range or CIDR is allowed. '
+                            'Specify multiple hosts by delimiting with spaces '
+                            'or commas or some (evil) mixture thereof. Ex: '
+                            '10.0.0.3-10.0.1.252,10.1.1.1 10.4.4.1-10.4.4.2'))
+    parser.add_argument(
+        '--transport',
+        choices=[
+            'tcp',
+            'udp',
+            'icmp',
+            'syn'],
+        default=['tcp'],
+        nargs='+')
+    parser.add_argument(
+        '-p',
+        '--ports',
+        nargs='+',
+        help=('Ports to scan. Specify as many as desired. Multiple ranges '
+              'are accepted. Mix them up as desired. Please delimit them '
+              'with commas and/or whitespace.'),
+        default=DEFAULT_PORTS)
+    parser.add_argument(
+        '-r',
+        '--report',
+        choices=[
+            'pdf',
+            'csv'],
+        nargs='+',
+        help='Save results to a file. Requires the "-o" flag')
+    parser.add_argument(
+        '-s',
+        '--save-as',
+        help='File name to save results as',
+        default='scan results')
+
+    args = parser.parse_args()
+
+    if pathsep == ':':
+        pathsep = '/'
+
+    if not args.input_file and not args.targets:
+        print('No targets specified. I don\'t know what you expected...')
+        exit(1)
+    if args.report and args.save_as == 'scan results':
+        print(
+            'No destination file specified. Will use default of "scan results"'
+        )
+    if args.save_as and args.save_as != 'scan results' and not args.report:
+        print(('A file to save to was specified but no format was passed. '
+               'Exiting so you may remedy this.'))
+        exit(1)
+    if args.save_as and args.report:
+        outfile_path, outfile_name = abspath(args.save_as).rsplit(pathsep, 1)
+    if args.input_file:
+        with open(abspath(args.input_file)) as f:
+            targets = parse_targets(f.readlines())
+    if args.targets:
+        targets = parse_targets(args.targets)
+    # Remove duplicates
+    targets = list(set(targets))
+
+    all_results = []
+    ports = parse_ports(args.ports)
+    for choice in args.transport:
+        if choice == 'icmp':
+            all_results.append(
+                (choice, scan_actions[choice](
+                    targets, ports, verbose=False)))
+        else:
+            results = []
+            for target in targets:
+                results.append(
+                    (target, scan_actions[choice](
+                        target, ports, verbose=False)))
+            all_results.append((choice, results))
+
+    report = defaultdict(dict)
+
+    for method, results in all_results:
+        if method == 'icmp':
+            print('The following devices were reachable via ICMP:')
+            alive = results[0]
+            for ip in alive:
+                print(ip)
+            if args.report and 'csv' in args.report:
+                with open(
+                        outfile_path + pathsep + 'icmp_' + outfile_name,
+                        'w') as f:
+                    f.writelines(list(map(lambda a: a + '\n', alive)))
+            if args.report and 'pdf' in args.report:
+                print('Sorry, ICMP PDF reports not available at this time!')
+            print()
+        else:
+            print(
+                'The following devices had these ports open when scanning',
+                method.upper() + ':')
+            pdf_compile = []
+            csv_compile = []
+            for target, successes in results:
+                if successes:
+                    print(' - Host', target +
+                          ':', ', '.join([str(port) for port, _ in successes]))
+                    if method != 'udp':
+                        pdf_compile.append([success[1]
+                                            for success in successes])
+                    csv_compile.append(
+                        ','.join(
+                            [target] + [str(success[0])
+                                        for success in successes]
+                        ) + '\n')
+
+            if args.report and 'pdf' in args.report and pdf_compile:
+                SndRcvList(reduce(lambda x, y: x + y, pdf_compile)).pdfdump(
+                    ''.join(
+                        [
+                            outfile_path,
+                            pathsep,
+                            method,
+                            '_',
+                            outfile_name,
+                            '.pdf']))
+
+            if args.report and 'csv' in args.report and csv_compile:
+                with open(
+                        ''.join([
+                            outfile_path,
+                            pathsep,
+                            method,
+                            '_',
+                            outfile_name,
+                            '.csv']), 'w') as f:
+                    f.writelines(csv_compile)
